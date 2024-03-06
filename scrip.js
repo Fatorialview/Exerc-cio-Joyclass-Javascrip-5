f02198997f5a79d741603662e462504c41398f23
Flavio <flaviosgp26@yahoo.com.br>
2024-03-06 10:52:44 -0300

    Subindo exercicio Javascrip Proz

---
+++ scrip.js
@@ -0,0 +1,28 @@
+let elementoH1 = document.querySelector("h1")
+let elementoA = document.querySelector("a")
+let elementoUL = document.querySelector("ul")
+elementoUL.innerHTML = `
+<ul> JavaScrip 1</ul>
+<ul> JavaScrip 2</ul>
+<ul> JavaScrip 3</ul>
+`
+
+let elementoOL = document.querySelector("ol")
+elementoOL.innerHTML = `
+<ol> Aprender 1</ol>
+<ol> Aprender 2</ol>
+<ol> Aprender 3</ol>
+`
+
+
+console.log(elementoH1);
+console.log(elementoH1.innerText);
+
+console.log(elementoA);
+console.log(elementoA.innerText);
+
+console.log(elementoUL);
+
+
+
+console.log(elementoOL);
